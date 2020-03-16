7066fb2ed6f0df9f925f7a245e8f116c5d3d73a1
Kirill Adeshenko <ka@tennisi.it>
2020-03-16 16:07:11 +0300

    Add connectivit store test and delete deprecated method from DI

--- lib/di/injector.dart
+++ lib/di/injector.dart
@@ -1,11 +1,5 @@
-import 'package:connectivity/connectivity.dart';
 import 'package:kiwi/kiwi.dart';
-import 'package:skill_branch_flutter/base/base_bloc.dart';
 import 'package:skill_branch_flutter/blocs/blocs.dart';
-import 'package:skill_branch_flutter/network/api/apis.dart';
-import 'package:skill_branch_flutter/redux/store.dart';
-import 'package:skill_branch_flutter/redux/user/user_actions.dart';
-
 
 class Injector {
   void configure() {
@@ -17,8 +11,6 @@ class Injector {
 
 class FeedService {
   FeedBloc feedBloc = FeedBloc();
-  final _api = PhotosApi();
-
 
   void initService() {
     feedBloc.init();
@@ -27,30 +19,12 @@ class FeedService {
   void killServicce() {
     feedBloc.dispose();
   }
-
-  Future<bool> likePhoto(String id, bool isLiked) async {
-
-    if (store.state.userState.isAuth) {
-      var connectivityResult = await Connectivity().checkConnectivity();
-
-      if (connectivityResult == ConnectivityResult.none)
-        throw StreamError.connectionError;
-      else
-        return await _api.likeUnlikePhoto(id, isLiked);
-      
-    } else {
-      store.dispatch(AuthUserAction());
-    }
-
-    return isLiked;
-  }
-
 }
-
 
 class BaseConfig {
   String baseUrl = 'https://api.unsplash.com';
-  String baseAccessKey = 'wbTuFeXl7HBB46OLvYni1lCGcESUeh0_SbpgMIXf2-k'; // 'm5dUsve-Aipa3w4RPn2YM7CokabnGNhCqXYFD2pIiPE';
+  String baseAccessKey =
+      'wbTuFeXl7HBB46OLvYni1lCGcESUeh0_SbpgMIXf2-k'; // 'm5dUsve-Aipa3w4RPn2YM7CokabnGNhCqXYFD2pIiPE';
   String baseSecretKey = 'y5ZY0m4RLJnL9K4mvazGLp5saixXOE8pGAGQH_9N8gU'; //'R1nDZUusBwvEgyEww_6HF2tR5uSePgTYTLYVf5JMuJw';
 }
 
---
+++ test/store/user.dart
@@ -0,0 +1,44 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:skill_branch_flutter/redux/app/app_reducer.dart';
+import 'package:skill_branch_flutter/redux/app/app_state.dart';
+import 'package:redux/redux.dart';
+import 'package:skill_branch_flutter/redux/connectivity/connectivity_actions.dart';
+import 'package:skill_branch_flutter/redux/navigation/navigation_middleware.dart';
+import 'package:skill_branch_flutter/redux/user/user_middleware.dart';
+import 'package:skill_branch_flutter/redux/user/user_state.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  group('Balance store test', () {
+    Store<AppState> store;
+
+    setUp(() async {
+      UserState userState;
+      store = Store(
+        appReducer,
+        initialState: AppState.initial(userState, ConnectivityResult.none),
+        distinct: true,
+        middleware: [
+          UserMiddleware(),
+          NavigationMiddleware(),
+        ],
+      );
+    });
+
+    test('test balance reducer', () {
+      expect(store.state.connectivityState.connectivityResult, ConnectivityResult.none);
+
+      store.dispatch(ConnectivityAction(ConnectivityResult.wifi));
+      expect(store.state.connectivityState.connectivityResult, ConnectivityResult.wifi);
+
+      store.dispatch(ConnectivityAction(ConnectivityResult.mobile));
+      expect(store.state.connectivityState.connectivityResult, ConnectivityResult.mobile);
+    });
+
+    tearDown(() {
+      store.teardown();
+    });
+  });
+}
